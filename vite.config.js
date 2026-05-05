1dd618e7f34183d9e60c6d11ad9ffb77477f9948
꧁ᙅᗣҜӠᙃ꧂ <real_cak3d@yahoo.com>
2026-05-05 09:48:32 -0400

    Remove vite-plugin-restart import from config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import 'dotenv/config'
-import restart from 'vite-plugin-restart'
 import wasm from 'vite-plugin-wasm'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import basicSsl from '@vitejs/plugin-basic-ssl'
